963b010a4c2c16e264ea3edee48fd328f61de938
chiyum <pray870712@gmail.com>
2024-08-17 17:14:25 +0800

    feat: 🎸 shop base style

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Progress from "@/components/progress.vue";
+import GradientBorderBox from "@/components/gradient-border-box.vue";
 import { useI18n } from "@/i18n";
+import { FAKE_SHOP_LIST } from "@/contants/shop";
 
 defineOptions({
   layout: "layout-shop"
@@ -12,12 +14,28 @@ interface State {
   amount: number;
   tip: string;
   level: number;
+  shopList: {
+    id: number;
+    buyPrice: number;
+    point: number;
+    discountPoint: number;
+    img: string;
+    imgSize: {
+      width: string;
+      height: string;
+      marginTop?: string;
+      marginRight?: string;
+    };
+    gemPoint: number;
+    vipPoint: number;
+  }[];
 }
 
 const state: State = reactive({
   amount: 450,
   tip: "",
-  level: 2
+  level: 2,
+  shopList: FAKE_SHOP_LIST
 });
 
 const init = (): void => {
@@ -52,10 +70,45 @@ init();
         </div>
       </div>
     </div>
-    <div class="shop-main"></div>
+    <div class="shop-main">
+      <GradientBorderBox
+        height="1.8rem"
+        main-class="shop-main-item"
+        v-for="buyPlan in state.shopList"
+        :key="buyPlan.id"
+      >
+        <div class="shop-main-item-top">
+          <div class="shop-main-item-top-info">
+            <q-img :src="buyPlan.img" :style="buyPlan.imgSize"></q-img>
+            <div class="shop-main-item-top-info-amount">
+              <div class="shop-main-item-top-info-amount-decoration">
+                {{ new Intl.NumberFormat().format(buyPlan.point) }}
+              </div>
+              <div class="shop-main-item-top-info-amount-final">
+                {{ new Intl.NumberFormat().format(buyPlan.discountPoint) }}
+              </div>
+            </div>
+          </div>
+          <div class="shop-main-item-top-btn">
+            <span> ${{ buyPlan.buyPrice }} </span>
+          </div>
+        </div>
+        <div class="shop-main-item-bottom">
+          <div class="shop-main-item-bottom-tip">{{ t("pages.shop.tip") }}</div>
+          <div class="shop-main-item-bottom-vip">
+            <q-img src="@/assets/images/shop/shop_vip.svg"></q-img>
+            <span>{{ buyPlan.gemPoint }}</span>
+          </div>
+          <div class="shop-main-item-bottom-gem">
+            <q-img src="@/assets/images/home/gem.svg"></q-img>
+            <span>{{ buyPlan.gemPoint }}</span>
+          </div>
+        </div>
+      </GradientBorderBox>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import "@/assets/scss/shop.scss";
 </style>
