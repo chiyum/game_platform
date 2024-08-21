d1172b0e5736cd0e6e85b5f62e5353dd17576ab1
George(chiyum) <pray870712@gmail.com>
2024-08-21 14:54:40 +0800

    fix: 🐛 設定彈窗缺少border-top補上

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "2ac02e7068d27b2491f17998c4fa1d8d"
   }, {
     "url": "index.html",
-    "revision": "0.fmq0pbc8tdg"
+    "revision": "0.sqhocdd0m3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
