f016c139c7e03df1a5e8920b8ca2e989117b1a1b
George(chiyum) <pray870712@gmail.com>
2024-08-22 19:58:41 +0800

    fix: 🐛 shop item style bug

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "2ac02e7068d27b2491f17998c4fa1d8d"
   }, {
     "url": "index.html",
-    "revision": "0.69mktcd9jro"
+    "revision": "0.v5t20bmqhpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
